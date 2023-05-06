60412b4970c2a3151f86e347891d5b6a86ae603d
Coss <124317426+Coss111@users.noreply.github.com>
2023-05-06 00:08:06 -0400

    FirstCommit
    :>

---
+++ internshipProject/internshipProject/src/test/java/bo/edu/ucb/internshipProject/InternshipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.internshipProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
